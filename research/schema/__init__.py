cd6b7a19453c8f7320017de2954dc1387cec7b71
gaixen <sohamjee1447@gmail.com>
2025-12-31 01:57:05 +0530

    [CI] intiate changes

--- research/schema/__init__.py
+++ research/schema/__init__.py
@@ -1,1 +1,24 @@
 """Research schema package."""
+
+from .auth import (
+    UserCreate,
+    UserLogin,
+    UserUpdate,
+    UserResponse,
+    AuditLogResponse,
+    RoleAssignment,
+    PermissionCheck,
+    PasswordChange,
+)
+
+
+__all__ = [
+    "UserLogin",
+    "UserUpdate",
+    "UserCreate",
+    "UserResponse",
+    "PermissionCheck",
+    "PasswordChange",
+    "RoleAssignment",
+    "AuditLogResponse"
+]
