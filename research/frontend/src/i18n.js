8f98b6b5e8a57bb760ace135309631d0889b1ead
sgnayak12 <gnayaksuhas12@gmail.com>
2026-01-31 17:11:45 +0000

    feat(i18n):added portuguese language

--- research/frontend/src/i18n.js
+++ research/frontend/src/i18n.js
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './i18n/en.json';
 import es from './i18n/es.json';
+import pt from './i18n/pt.json'
 
 i18n
 // .use(LanguageDetector)
@@ -11,6 +12,7 @@ i18n
   resources: {
     en: { translation: en },
     es: { translation: es },
+    pt: { translation: pt }
   },
   lng: 'en',
   fallbackLng: 'en',
